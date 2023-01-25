f140338f30a3ad11e42e5316834eafa2f630e91a
M S I <M S I@MSI>
2023-01-25 14:53:57 +0700

    add viuw login

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Login"),
+        ),
+      ),
+    );
   }
 }
