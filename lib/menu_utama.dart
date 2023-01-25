b9b784422182e0bb874cd0f8853ab74b28b0eb00
M S I <M S I@MSI>
2023-01-25 16:55:37 +0700

    add class menu utama

---
+++ lib/menu_utama.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Menu_Utama extends StatelessWidget {
+  const Menu_Utama({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
