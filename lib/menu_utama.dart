09a16fb7ddb2196e508a8bfa845bc04675ca41f7
M S I <M S I@MSI>
2023-01-26 21:25:47 +0700

    add icons setting, Search, Riwayat, home, appInfo, Help and logOut to drawer

--- lib/menu_utama.dart
+++ lib/menu_utama.dart
@@ -1,3 +1,4 @@
+import 'package:final_project_2023/page_login.dart';
 import 'package:flutter/material.dart';
 
 class Menu_Utama extends StatelessWidget {
@@ -21,7 +22,7 @@ class Menu_Utama extends StatelessWidget {
               color: Colors.redAccent,
               alignment: Alignment.bottomLeft,
               child: Text(
-                "Menu",
+                "All Menu",
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 25,
@@ -32,8 +33,101 @@ class Menu_Utama extends StatelessWidget {
               height: 20,
             ),
             ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
+              leading: Icon(
+                Icons.home,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Home",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.search,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Search",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.notifications,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Notification",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.info,
+                size: 25,
+                color: Colors.red,
+              ),
+              title: Text(
+                "Apss Info",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.update_sharp,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Riwayat",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.wallet,
+                size: 25,
+                color: Colors.blue,
+              ),
+              title: Text(
+                "Isi Dompet",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Setting",
+              ),
+            ),
+            ListTile(
+              onTap: (() {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Page_Login()),
+                  ),
+                );
+              }),
+              leading: Icon(
+                Icons.logout,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Logout",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.help,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Help & feadback",
+              ),
             ),
           ],
         ),
