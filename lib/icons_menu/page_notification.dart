60459f754a6d2222465612382af0f2d1815a3476
M S I <M S I@MSI>
2023-01-27 14:31:46 +0700

    add icons notifikasi sisstem to page notification some widget listile

--- lib/icons_menu/page_notification.dart
+++ lib/icons_menu/page_notification.dart
@@ -1,3 +1,11 @@
+import 'dart:ui';
+
+import 'package:final_project_2023/icons_menu/apps_info.dart';
+import 'package:final_project_2023/icons_menu/help_facback.dart';
+import 'package:final_project_2023/icons_menu/riwayat.dart';
+import 'package:final_project_2023/icons_menu/setting.dart';
+import 'package:final_project_2023/menu_utama.dart';
+import 'package:final_project_2023/page_login.dart';
 import 'package:flutter/material.dart';
 
 class Page_Notification extends StatelessWidget {
@@ -6,8 +14,193 @@ class Page_Notification extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.forward_to_inbox_outlined,
+              size: 25,
+              color: Colors.black,
+            ),
+            title: Text(
+              "Notifikasi Sistem",
+            ),
+          ),
+        ],
+      ),
       appBar: AppBar(
-        title: Text("Notification"),
+        title: Text(
+          "Setting",
+        ),
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              width: double.infinity,
+              height: 150,
+              color: Colors.redAccent,
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "All Menu",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Menu_Utama()),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.home,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Home",
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.search,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Search",
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Page_Notification()),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.notifications,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Notification",
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Apps_Info()),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.info,
+                size: 25,
+                color: Colors.red,
+              ),
+              title: Text(
+                "Apss Info",
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Riwayat()),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.update_sharp,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Riwayat",
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Menu_Utama()),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.wallet,
+                size: 25,
+                color: Colors.blue,
+              ),
+              title: Text(
+                "Isi Dompet",
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Setting()),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.settings,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Setting",
+              ),
+            ),
+            ListTile(
+              onTap: (() {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Page_Login()),
+                  ),
+                );
+              }),
+              leading: Icon(
+                Icons.logout,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Logout",
+              ),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: ((context) => Help_Facback()),
+                  ),
+                );
+              },
+              leading: Icon(
+                Icons.help,
+                size: 25,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Help & feadback",
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
