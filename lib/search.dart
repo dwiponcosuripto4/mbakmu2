5cb3e7c69129965c96fe28ab85bb7e986ff84711
M S I <M S I@MSI>
2023-01-29 03:36:21 +0700

    add class search

---
+++ lib/search.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Serach_Page extends StatelessWidget {
+  const Serach_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
